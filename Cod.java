f07ae693f0ab21f0dd3866e1c8ad113b07d6894e
AlekhinVladimir <Alehin.v.m@mail.com>
2023-10-30 16:01:22 +0400

    change cod

--- Cod.java
+++ Cod.java
@@ -1,5 +1,6 @@
 class Hello {
     public static void main(String[] args) {
         System.out.println("Hellooooooo, World!"); /// Отличие которое приняли от удаленного репозитория
+        System.out.println("белиберда");
     }
 }
